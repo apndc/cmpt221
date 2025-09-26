c10f739387e9e8eb04b4d1b7b353e1380c829963
Amelia Nelson <amelianelson@Amelias-MacBook-Air.local>
2025-09-25 21:22:14 -0400

    completed lab 4

--- labs/lab-4/db/sql.py
+++ labs/lab-4/db/sql.py
@@ -29,6 +29,10 @@ def insert_courses():
     try:
         # TODO: write a SQL query to insert 3 records 
         query = """
+        INSERT INTO "Courses" ("CourseName", "Semester", "Year") VALUES
+        ('Intro to Programming', 'Fall', '2024'),
+        ('Database Management', 'Spring', '2024'),
+        ('Software Develepment 2', 'Fall', '2025');
         """
         session.execute(text(query))
         session.commit()
@@ -46,6 +50,7 @@ def update_course():
     try:
         # TODO: write a SQL query to update 1 record
         query = """
+        UPDATE "Courses" SET "CourseName" = 'Advanced Database Systems' WHERE "CourseID" = 10;
         """
         result = session.execute(text(query))
         # "save" the changes
@@ -65,6 +70,7 @@ def delete_course():
     try:
         # TODO: write a SQL query to delete 1 record
         query = """
+        DELETE FROM "Courses" WHERE "CourseID" = 4;
         """
         result = session.execute(text(query))
         # "save" the changes
